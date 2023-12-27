eb6759b8ed709f84c8e63054ac6c5753e87e18d6
SeNriZzZ <andrijko2703@gmail.com>
2023-12-27 14:19:06 +0200

    firebase removed

--- Assets/Scripts/DatabaseReader.cs
+++ Assets/Scripts/DatabaseReader.cs
@@ -2,36 +2,38 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Firebase;
-using Firebase.Database;
-using Firebase.Extensions;
+// using Firebase;
+// using Firebase.Database;
+// using Firebase.Extensions;
 
 public class DatabaseReader : MonoBehaviour
 {
-    private DatabaseReference _reference;
-    private string urlValue;
+    // private DatabaseReference _reference;
+    private string urlValue = "https://www.google.com";
 
     public event Action OnURLFetched;
 
     public string URLValue => urlValue;
     private void Start() 
     {
-        _reference = FirebaseDatabase.DefaultInstance.RootReference;
-        GetURL();
+        //_reference = FirebaseDatabase.DefaultInstance.RootReference;
+        //GetURL();
+        OnURLFetched?.Invoke();
     }
 
     
     private IEnumerator GetURLRoutine()
     {
-        var url = _reference.Child("URL").GetValueAsync();
-        yield return new WaitUntil(predicate: () => url.IsCompleted);
-
-        if (url != null)
-        {
-            var snapShot = url.Result;
-            urlValue = snapShot.Value.ToString();
-            OnURLFetched?.Invoke();
-        }
+        // var url = _reference.Child("URL").GetValueAsync();
+        // yield return new WaitUntil(predicate: () => url.IsCompleted);
+        //
+        // if (url != null)
+        // {
+        //     var snapShot = url.Result;
+        //     urlValue = snapShot.Value.ToString();
+        //     OnURLFetched?.Invoke();
+        // }
+        yield break;
     }
 
     private void GetURL()
